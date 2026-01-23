dbf893581d631dd02ca5b32b3b0d8961e04d1d27
Andres Rojas <afrojas@gmail.com>
2026-01-23 09:41:51 -0800

    slimming it down

--- lib/rails_sql_prettifier/protected_env.rb
+++ lib/rails_sql_prettifier/protected_env.rb
@@ -6,11 +6,8 @@ module RailsSQLPrettifier
       migration_context = ActiveRecord::Base.connection.try(:migration_context) ||
         ActiveRecord::Base.connection.try(:pool)&.migration_context # rails 7.2+
 
-      if ActiveRecord::Base.connection.database_exists?
-        migration_context&.protected_environment? ||
-          defined?(Rails) && !(Rails.env.test? || Rails.env.development?)
-      else
-        false
+      (ActiveRecord::Base.connection.database_exists? && migration_context&.protected_environment?) ||
+        defined?(Rails) && ActiveRecord::Base.protected_environments.include?(Rails.env)
       end
     end
   end
