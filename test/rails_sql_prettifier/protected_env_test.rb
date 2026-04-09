8d7f12cdd9dbb7eb613d40cf4ff79613d4429fba
Andrés Lasser-Rojass <andres.lasser-rojas@hover.to>
2026-04-09 09:45:15 -0700

    Simplify protected_env_test per review feedback

--- test/rails_sql_prettifier/protected_env_test.rb
+++ test/rails_sql_prettifier/protected_env_test.rb
@@ -3,22 +3,9 @@
 require_relative "../test_helper"
 
 class ProtectedEnvTest < ActiveSupport::TestCase
-  extend ::ActiveSupport::Testing::Declarative
-
-  class TestClass
-    include RailsSQLPrettifier::ProtectedEnv
-  end
-
-  def setup
-    @test_instance = TestClass.new
-  end
-
   test "protected_env? returns false when database does not exist" do
-    connection = ActiveRecord::Base.connection
-
-    connection.stub(:database_exists?, false) do
-      assert_equal false, @test_instance.protected_env?
+    ActiveRecord::Base.connection.stub(:database_exists?, false) do
+      assert_equal false, Niceql.protected_env?
     end
   end
-
 end
